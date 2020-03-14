5bf3c2a5c9a6e22a01a4637d25b0165a1be272b8
Wasim <54527077+SerpentByte@users.noreply.github.com>
2020-03-14 17:12:08 +0530

    Create firewall_nodes.sh

---
+++ firewall_nodes.sh
@@ -0,0 +1,4 @@
+firewall-cmd --permanent --add-port=6783/tcp
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10255/tcp
+firewall-cmd --permanent --add-port=30000-32767/tcp
