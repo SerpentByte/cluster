36ff7f0daac0db3aed79d4be7c1af5dcdf6c4e7a
Wasim <54527077+SerpentByte@users.noreply.github.com>
2020-03-14 12:14:58 +0530

    Update join_nodes.sh

--- join_nodes.sh
+++ join_nodes.sh
@@ -1,2 +1,2 @@
-kubeadm join 10.10.10.200:6443 --token ep513w.jjfk8fpblkigjitk\
---discovery-toekn-ca-cert-hash sha256:b3ba9032f104215f1c7d848e436564437ae7dfc416434301f1d7334d71c6ef1d
+swapoff -a
+kubeadm join 10.10.10.200:6443 --token ep513w.jjfk8fpblkigjitk --discovery-token-ca-cert-hash sha256:b3ba9032f104215f1c7d848e436564437ae7dfc416434301f1d7334d71c6ef1d
